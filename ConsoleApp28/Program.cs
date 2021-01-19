bbc05a76af66efee27864b106eb09dc3259b4d87
fsafdsa Lfybkf <P.danila228@mail.ru>
2021-01-19 12:37:34 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp28/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "привет";
+            string s2 = "ПРИВЕТ";
+            Console.WriteLine(s1.ToUpper());
+            Console.WriteLine(s2.ToLower());
+            
+            Console.ReadLine();
+
+        }
+    }
+}
